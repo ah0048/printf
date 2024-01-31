a035f8812031ca6e045563dc425f0f14f70166d3
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-01-30 17:09:06 -0800

    edit

--- tests/main.c
+++ tests/main.c
@@ -17,5 +17,11 @@ int main(void)
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
     return (0);
 }
